27c86ad90fc694ba66d04c7691b0f2a194073bc8
Daniel Hayden <dan@sledro.com>
2017-04-18 11:17:32 +0100

    New ionic 2 firebase framework

---
+++ src/app/app.module.ts
@@ -0,0 +1,52 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+
+// Pages
+import { ForgotPasswordPage } from '../pages/auth/forgot-password/forgot-password';
+import { AuthPage } from '../pages/auth/home/home';
+import { LoginEmailPage } from '../pages/auth/login-email/login-email';
+import { SignUpPage } from '../pages/auth/sign-up/sign-up';
+import { HomePage } from '../pages/home/home';
+import { TermsOfServicePage } from '../pages/terms-of-service/terms-of-service';
+
+import { AngularFireModule } from 'angularfire2';
+
+// Providers
+import { DataProvider } from '../providers/data';
+import { AuthProvider } from '../providers/auth';
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDcbsUEReGm_dlijVXC1sMCcqKpCsXt0nQ',
+  authDomain: 'ionic2-angularfire-login-14ea3.firebaseapp.com',
+  databaseURL: 'https://ionic2-angularfire-login-14ea3.firebaseio.com',
+  storageBucket: 'ionic2-angularfire-login-14ea3.appspot.com',
+};
+
+@NgModule({
+  declarations: [
+    MyApp,
+    ForgotPasswordPage,
+    AuthPage,
+    LoginEmailPage,
+    SignUpPage,
+    HomePage,
+    TermsOfServicePage
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    ForgotPasswordPage,
+    AuthPage,
+    LoginEmailPage,
+    SignUpPage,
+    HomePage,
+    TermsOfServicePage
+  ],
+  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}, DataProvider, AuthProvider]
+})
+export class AppModule {}
