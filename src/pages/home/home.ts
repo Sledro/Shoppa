1abfd6d8d49f33eb8b9671cf812d87570af6f839
Daniel Hayden <dan@sledro.com>
2017-04-18 17:19:57 +0100

    Logout button and function added.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AuthProvider } from '../../providers/auth';
 
 @Component({
   templateUrl: 'home.html',
   selector: 'home',
 })
 export class HomePage {
-  constructor(private nav: NavController) {
+  constructor(private nav: NavController, private auth: AuthProvider) {
 
   }
+
+logout(){
+this.auth.logout();
+}
+
 }
